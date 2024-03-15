4abeed73425195dd15f6d73fbc0276d261eed752
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-03-15 14:08:00 -0400

    rough estimate of best max_depth

--- kaggle-house-prices/forest.py
+++ kaggle-house-prices/forest.py
@@ -14,7 +14,7 @@ fold1, fold2, fold3, fold4, fold5 = kf.split(X)
 folds = [fold1, fold2, fold3, fold4, fold5]
 
 # set up model
-regr = RandomForestRegressor(max_depth=2, random_state=117)
+regr = RandomForestRegressor(max_depth=10, random_state=117)
 error = []
 
 # cross val loop
