f691c36606b7e5695a8bcae082e871b340f71dc4
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-03-18 13:41:55 -0400

    use engineered csv in xgb.py

--- kaggle-house-prices/xgb.py
+++ kaggle-house-prices/xgb.py
@@ -5,10 +5,10 @@ from sklearn.model_selection import KFold
 import xgboost as xgb
 
 # set up data
-data = pd.read_csv('train.csv')
-numeric = data.select_dtypes(include=np.number).drop(columns='Id')
-X = numeric.drop(columns='SalePrice')
-y = numeric['SalePrice']
+data = pd.read_csv('train_num.csv')
+#numeric = data.select_dtypes(include=np.number).drop(columns='Id')
+X = data.drop(columns='SalePrice')
+y = data['SalePrice']
 kf = KFold(n_splits=5, shuffle=True, random_state=117)
 fold1, fold2, fold3, fold4, fold5 = kf.split(X)
 folds = [fold1, fold2, fold3, fold4, fold5]
