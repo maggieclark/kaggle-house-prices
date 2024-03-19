69ff82311d7d46dd0c07bef103c25afc1903d089
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-03-19 16:39:26 -0400

    fix formatting

--- kaggle-house-prices/feature_eng.py
+++ kaggle-house-prices/feature_eng.py
@@ -15,14 +15,14 @@ data_eng = data.replace(['Po', 'Fa', 'TA', 'Gd', 'Ex'], [-2, -1, 0, 1, 2])
 # code ordinals - individual columns
 data_eng = data_eng.replace({'Alley': {'Grvl': 1, # Alley includes NaN if not gravel or paved
                                    'Pave': 1},
-                         'LotShape': {'Reg': 0,
+                             'LotShape': {'Reg': 0,
                                       'IR1': 1,
                                       'IR2': 2,
                                       'IR3': 3},
-                         'LandSlope': {'Gtl': 1, # LandSlope includes no missings
+                             'LandSlope': {'Gtl': 1, # LandSlope includes no missings
                                        'Mod': 2,
                                        'Sev': 3},
-                         'Condition1': {'PosA': 2,
+                             'Condition1': {'PosA': 2,
                                         'PosN': 1,
                                         'Norm': 0,
                                         'RRNe': -1,
@@ -31,7 +31,7 @@ data_eng = data_eng.replace({'Alley': {'Grvl': 1, # Alley includes NaN if not gr
                                         'RRAn': -2,
                                         'Artery': -2,
                                         'Feedr': -2},
-                         'Condition2': {'PosA': 2,
+                             'Condition2': {'PosA': 2,
                                         'PosN': 1,
                                         'Norm': 0,
                                         'RRNe': -1,
@@ -40,7 +40,7 @@ data_eng = data_eng.replace({'Alley': {'Grvl': 1, # Alley includes NaN if not gr
                                         'RRAn': -2,
                                         'Artery': -2,
                                         'Feedr': -2},
-                         'HouseStyle': {'1Story': 1,
+                             'HouseStyle': {'1Story': 1,
                                         '1.5Unf': 2,
                                         '1.5Fin': 3,
                                         '2Story': 4,
@@ -48,24 +48,24 @@ data_eng = data_eng.replace({'Alley': {'Grvl': 1, # Alley includes NaN if not gr
                                         '2.5Fin': 6,
                                         'SFoyer': np.nan,
                                         'SLevel': np.nan},
-                         'BsmtExposure': {'No': 0,
+                             'BsmtExposure': {'No': 0,
                                           'Mn': 1,
                                           'Av': 2,
                                           'Gd': 3},
-                         'BsmtFinType1': {'Unf': 0,
+                             'BsmtFinType1': {'Unf': 0,
                                           'LwQ': 1,
                                           'Rec': 2,
                                           'BLQ': 3,
                                           'ALQ': 4,
                                           'GLQ': 5},
-                         'CentralAir': {'N': 0,
+                             'CentralAir': {'N': 0,
                                         'Y': 1},
-                         'Electrical': {'SBrkr': 1,
+                             'Electrical': {'SBrkr': 1,
                                         'FuseA': 0,
                                         'FuseF': -1,
                                         'FuseP': -2,
                                         'Mix': -1}, # setting Mix to same as Fair
-                         'Functional': {'Typ': 0,
+                             'Functional': {'Typ': 0,
                                         'Min1': -1,
                                         'Min2': -2,
                                         'Mod': -3,
@@ -73,10 +73,10 @@ data_eng = data_eng.replace({'Alley': {'Grvl': 1, # Alley includes NaN if not gr
                                         'Maj2': -5,
                                         'Sev': -6,
                                         'Sal': -7},
-                         'GarageFinish': {'Unf': 1,
+                             'GarageFinish': {'Unf': 1,
                                           'RFn': 2,
                                           'Fin': 3}
-                        })
+                             })
 
 # code ordinals  - requiring conditions
 data_eng['YearBuilt'] = data_eng['YearBuilt'].mask(data_eng['YearBuilt'] < 1900, 1899) # earliest is 1972
@@ -95,8 +95,13 @@ data_num.to_csv('train_num.csv')
 
 ## select and engineer continuous ##
 
+# replace quality measures with quality sum which will be somewhat continuous
+data_cont = data.replace(['Po', 'Fa', 'TA', 'Gd', 'Ex'], [-2, -1, 0, 1, 2])
+
+
 # replace neighborhood name with median income
 
-# replace quality measures with quality sum which will be somewhat continuous
 
 # select continuous variables
+
+
