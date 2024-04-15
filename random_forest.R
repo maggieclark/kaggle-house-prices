760b9bb29485f25183e039bfe662bf7d096b56d7
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-04-15 14:30:00 -0400

    using new nhood vars

--- random_forest.R
+++ random_forest.R
@@ -354,3 +354,102 @@ mean(metrics)
 # importance plot
 varImpPlot(rf2)
 
+######### train_cleaned_option2.csv with extra nhood vars ###############################
+
+# read data
+data = read_csv('train_cleaned_option2.csv') %>% 
+  select(!c('...1', 'Id'))
+
+# divide into 5 folds
+fold_assn <- sample(c('fold1', 'fold2', 'fold3', 'fold4', 'fold5'), nrow(data), replace=TRUE)
+data = cbind(data, fold_assn)
+
+
+### imputed medians ###
+model1data = data %>% 
+  mutate(across(where(is.character), factor)) # convert categoricals to factors
+
+metrics = c()
+
+for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
+  print(f)
+  
+  datasets = fold_ttsplit(model1data, "SalePrice", f)
+  print('datasets created')
+  
+  rf1 = randomForest(na.roughfix(datasets[[1]]),
+                     datasets[[2]]$SalePrice,
+                     xtest = na.roughfix(datasets[[3]]),
+                     ytest = datasets[[4]]$SalePrice,
+                     ntree=100, 
+                     importance=T)
+  print('forest created')
+  
+  test_len = nrow(datasets[[4]])
+  
+  SSE = 0
+  
+  for (i in 1:test_len){
+    ith_SE = (log(rf1[['test']]$predicted[i]) - log(datasets[[4]]$SalePrice[i]))^2
+    SSE = SSE + ith_SE
+  }
+  print('error calculated')
+  
+  metrics = append(metrics, sqrt(SSE/test_len))
+}
+
+metrics
+
+# mean: 0.1481815
+mean(metrics)
+
+# importance
+
+varImpPlot(rf1)
+
+
+### missing as factor level ###
+model2data = data %>% 
+  mutate(across(where(is.character), \(x) replace_na(x, "unknown"))) %>% 
+  mutate(across(where(is.numeric), \(x) replace_na(x, -99)))
+
+model2data = model2data %>% 
+  mutate(across(where(is.character), factor)) # convert categoricals to factors
+
+metrics = c()
+
+for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
+  
+  datasets = fold_ttsplit(model2data, "SalePrice", f)
+  print('datasets created')
+  
+  rf2 = randomForest(datasets[[1]],
+                     datasets[[2]]$SalePrice,
+                     xtest = datasets[[3]],
+                     ytest = datasets[[4]]$SalePrice,
+                     ntree=500, 
+                     importance=T,
+                     keep.forest = T)
+  
+  print('forest created')
+  
+  test_len = nrow(datasets[[4]])
+  
+  SSE = 0
+  
+  for (i in 1:test_len){
+    ith_SE = (log(rf2[['test']]$predicted[i]) - log(datasets[[4]]$SalePrice[i]))^2
+    SSE = SSE + ith_SE
+  }
+  print('error calculated')
+  
+  metrics = append(metrics, sqrt(SSE/test_len))
+}
+
+# mean of missing as factor level with feature engineered data: 0.1480007
+mean(metrics)
+
+# importance plot
+varImpPlot(rf2)
+
+
