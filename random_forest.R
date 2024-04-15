63d18c88f57f7c6da1c7e7e6d46f74657688a443
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-04-15 14:40:39 -0400

    eval with 500 trees

--- random_forest.R
+++ random_forest.R
@@ -75,7 +75,7 @@ for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
                      datasets[[2]]$SalePrice,
                      xtest = na.roughfix(datasets[[3]]),
                      ytest = datasets[[4]]$SalePrice,
-                     ntree=100, 
+                     ntree=500, 
                      importance=T)
   print('forest created')
   
@@ -94,7 +94,7 @@ for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
 
 metrics
 
-# mean of imputed medians with minimally processed data: 0.167349
+# mean of imputed medians with minimally processed data: 0.1672828
 mean(metrics)
 
 # importance
@@ -149,7 +149,7 @@ mean(metrics)
 varImpPlot()
 
 
-############ with more feature engineering ###########################
+############ with some feature engineering ###########################
 
 # read data
 data = read_csv('train_cleaned_option1.csv') %>% 
@@ -175,7 +175,7 @@ for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
                      datasets[[2]]$SalePrice,
                      xtest = na.roughfix(datasets[[3]]),
                      ytest = datasets[[4]]$SalePrice,
-                     ntree=100, 
+                     ntree=500, 
                      importance=T)
   print('forest created')
   
@@ -192,9 +192,7 @@ for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
   metrics = append(metrics, sqrt(SSE/test_len))
 }
 
-metrics
-
-# mean of imputed medians with feature eng data: 0.1641935
+# mean of imputed medians with feature eng data: 0.1620963
 mean(metrics)
 
 # importance
@@ -254,7 +252,9 @@ varImpPlot(rf2)
 
 varUsed(rf2) # columns 5 (Street), 9 (Utilities) and and 14 (Condition 2) were not used
 
-################## train_cleaned_option2.csv ###############################
+
+
+######### train_cleaned_option2.csv ###############################
 
 # read data
 data = read_csv('train_cleaned_option2.csv') %>% 
@@ -281,7 +281,7 @@ for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
                      datasets[[2]]$SalePrice,
                      xtest = na.roughfix(datasets[[3]]),
                      ytest = datasets[[4]]$SalePrice,
-                     ntree=100, 
+                     ntree=500, 
                      importance=T)
   print('forest created')
   
@@ -298,9 +298,7 @@ for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
   metrics = append(metrics, sqrt(SSE/test_len))
 }
 
-metrics
-
-# mean of imputed medians with feature eng data: 0.1529816
+# mean: 0.1475555
 mean(metrics)
 
 # importance
@@ -346,107 +344,7 @@ for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
   metrics = append(metrics, sqrt(SSE/test_len))
 }
 
-metrics
-
-# mean of missing as factor level with feature engineered data: 0.1516034
-mean(metrics)
-
-# importance plot
-varImpPlot(rf2)
-
-######### train_cleaned_option2.csv with extra nhood vars ###############################
-
-# read data
-data = read_csv('train_cleaned_option2.csv') %>% 
-  select(!c('...1', 'Id'))
-
-# divide into 5 folds
-fold_assn <- sample(c('fold1', 'fold2', 'fold3', 'fold4', 'fold5'), nrow(data), replace=TRUE)
-data = cbind(data, fold_assn)
-
-
-### imputed medians ###
-model1data = data %>% 
-  mutate(across(where(is.character), factor)) # convert categoricals to factors
-
-metrics = c()
-
-for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
-  print(f)
-  
-  datasets = fold_ttsplit(model1data, "SalePrice", f)
-  print('datasets created')
-  
-  rf1 = randomForest(na.roughfix(datasets[[1]]),
-                     datasets[[2]]$SalePrice,
-                     xtest = na.roughfix(datasets[[3]]),
-                     ytest = datasets[[4]]$SalePrice,
-                     ntree=100, 
-                     importance=T)
-  print('forest created')
-  
-  test_len = nrow(datasets[[4]])
-  
-  SSE = 0
-  
-  for (i in 1:test_len){
-    ith_SE = (log(rf1[['test']]$predicted[i]) - log(datasets[[4]]$SalePrice[i]))^2
-    SSE = SSE + ith_SE
-  }
-  print('error calculated')
-  
-  metrics = append(metrics, sqrt(SSE/test_len))
-}
-
-metrics
-
-# mean: 0.1481815
-mean(metrics)
-
-# importance
-
-varImpPlot(rf1)
-
-
-### missing as factor level ###
-model2data = data %>% 
-  mutate(across(where(is.character), \(x) replace_na(x, "unknown"))) %>% 
-  mutate(across(where(is.numeric), \(x) replace_na(x, -99)))
-
-model2data = model2data %>% 
-  mutate(across(where(is.character), factor)) # convert categoricals to factors
-
-metrics = c()
-
-for (f in c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')){
-  
-  datasets = fold_ttsplit(model2data, "SalePrice", f)
-  print('datasets created')
-  
-  rf2 = randomForest(datasets[[1]],
-                     datasets[[2]]$SalePrice,
-                     xtest = datasets[[3]],
-                     ytest = datasets[[4]]$SalePrice,
-                     ntree=500, 
-                     importance=T,
-                     keep.forest = T)
-  
-  print('forest created')
-  
-  test_len = nrow(datasets[[4]])
-  
-  SSE = 0
-  
-  for (i in 1:test_len){
-    ith_SE = (log(rf2[['test']]$predicted[i]) - log(datasets[[4]]$SalePrice[i]))^2
-    SSE = SSE + ith_SE
-  }
-  print('error calculated')
-  
-  metrics = append(metrics, sqrt(SSE/test_len))
-}
-
-# mean of missing as factor level with feature engineered data: 0.1480007
+# mean: 0.1480007
 mean(metrics)
 
 # importance plot
